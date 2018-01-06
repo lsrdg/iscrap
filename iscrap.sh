4207517c692d8df723959287de5565984d5752ab
lsrdg <lsrdg@protonmail.com>
2018-01-06 21:08:47 +0100

    Update 'iscrap.sh' to work with positional arguments.

--- iscrap.sh
+++ iscrap.sh
@@ -3,10 +3,10 @@
 
 if [ $# -eq 0 ]
 then
-    python ~/git/iscrap/iscrap.py --fetch ; sudo pacman -Syu;
+    python ~/git/iscrap/iscrap.py fetch 3; sudo pacman -Syu;
 
-elif [ $1 == '-r' ]; then
-    python ~/git/iscrap/iscrap.py --read $2;
+elif [ $1 == 'read' ]; then
+    python ~/git/iscrap/iscrap.py read $2;
 fi
 
 #http://ryanstutorials.net/bash-scripting-tutorial/bash-variables.php1
